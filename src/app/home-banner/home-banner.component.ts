7499eb3ee2b14676c57e347c42faea4848ac2a7f
Santhosh Vavilala <santhosh.vavilala@hotmail.com>
2021-02-22 18:41:28 +0530

    Add Link Card Component

--- src/app/home-banner/home-banner.component.ts
+++ src/app/home-banner/home-banner.component.ts
@@ -7,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeBannerComponent implements OnInit {
 
+  needSupport = {header: 'I am looking for Supoort', subHeader: 'Support starting as low as $10 per issue*'}
+  provideSupport = {header: 'I Can provide technical Supoort', subHeader: 'Best oppurtunity to get paid for your skills.'}
   constructor() { }
-
+  
   ngOnInit(): void {
   }
 
--- src/app/link-card/link-card.component.ts
+++ src/app/link-card/link-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-link-card',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./link-card.component.scss']
 })
 export class LinkCardComponent implements OnInit {
-
+  @Input()  linkInfo;
   constructor() { }
 
   ngOnInit(): void {
