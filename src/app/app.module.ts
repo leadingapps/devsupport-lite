dee894b9e13304b9a9cc5ed6f54abcfdcb0393ac
Santhosh Vavilala <santhosh.vavilala@hotmail.com>
2021-02-22 19:04:21 +0530

    Add Header Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeBannerComponent } from './home-banner/home-banner.component';
 import { LinkCardComponent } from './link-card/link-card.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeBannerComponent,
-    LinkCardComponent
+    LinkCardComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
