014021c4c59c7e89f3777aeb3838aeb071563edc
Santhosh Vavilala <santhosh.vavilala@hotmail.com>
2021-02-22 14:17:16 +0530

    Add home banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeBannerComponent } from './home-banner/home-banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeBannerComponent
   ],
   imports: [
     BrowserModule,
