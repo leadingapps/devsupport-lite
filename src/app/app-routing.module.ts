19d563823cdc4acaa10153f5a09ed1feab3c6f01
Santhosh Vavilala <santhosh.vavilala@hotmail.com>
2021-02-23 00:04:26 +0530

    Add privacy policy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeBannerComponent } from './home-banner/home-banner.component';
 import { NeedSupportComponent } from './need-support/need-support.component';
+import { PrivacyComponent } from './privacy/privacy.component';
 import { TermsComponent } from './terms/terms.component';
 
 
-const routes: Routes = [{path:'terms-conditions', component: TermsComponent}, {path:'', component: HomeBannerComponent},{path:'home', component: HomeBannerComponent}, {path:'need-support',component: NeedSupportComponent}];
+const routes: Routes = [{path:'privacy-policy', component: PrivacyComponent},{path:'terms-conditions', component: TermsComponent}, {path:'', component: HomeBannerComponent},{path:'home', component: HomeBannerComponent}, {path:'need-support',component: NeedSupportComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
