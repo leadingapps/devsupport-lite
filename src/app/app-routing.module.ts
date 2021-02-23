4c04e30c28180ebde7f7449eeba1afe7a3eea7ed
Santhosh Vavilala <santhosh.vavilala@hotmail.com>
2021-02-23 23:28:33 +0530

    Add Provide support page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,11 @@ import { AboutComponent } from './about/about.component';
 import { HomeBannerComponent } from './home-banner/home-banner.component';
 import { NeedSupportComponent } from './need-support/need-support.component';
 import { PrivacyComponent } from './privacy/privacy.component';
+import { ProvideSupportComponent } from './provide-support/provide-support.component';
 import { TermsComponent } from './terms/terms.component';
 
 
-const routes: Routes = [{path:'about-us', component: AboutComponent},{path:'privacy-policy', component: PrivacyComponent},{path:'terms-conditions', component: TermsComponent}, {path:'', component: HomeBannerComponent},{path:'home', component: HomeBannerComponent}, {path:'need-support',component: NeedSupportComponent}];
+const routes: Routes = [{ path: 'provide-support', component: ProvideSupportComponent }, { path: 'about-us', component: AboutComponent }, { path: 'privacy-policy', component: PrivacyComponent }, { path: 'terms-conditions', component: TermsComponent }, { path: '', component: HomeBannerComponent }, { path: 'home', component: HomeBannerComponent }, { path: 'need-support', component: NeedSupportComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
